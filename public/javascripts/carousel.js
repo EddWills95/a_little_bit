3b128c44d9cfc45c5c46525c088495fd9de5d77c
Edd Williams <edd.williams@me.com>
2017-06-29 21:19:54 +0100

    Working on the carousel

---
+++ public/javascripts/carousel.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+	loadCarousel();
+	slides[0].style.display = "block";
+}
+var slides = document.getElementsByClassName('slide');
+
+function loadCarousel() {
+	var index = 0;
+
+	resetSlides(index);
+
+	document.getElementById('nextButton').addEventListener("click", function() {
+		slides[index].classList.add('fadeOutRight');
+
+		
+
+		if (index == slides.length - 1) {
+			index = 0;
+		} else {
+			index++;
+		}
+
+		slides[index].style.display = "block";
+		slides[index].classList.add('fadeInLeft');
+
+	});
+}
+
+function resetSlides(current) {
+	for(var i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[current].style.display = "block";
+}
+
